e9b99aef00e833513a2b8ee3681dba14daf92930
Simonet <matthieu.simonet@iut-dijon.u-bourgogne.fr>
2025-07-15 15:10:57 +0200

    Ajout de l'import dans le composant Père

--- pocAngular/src/app/app.ts
+++ pocAngular/src/app/app.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { TopMenu } from './components/top-menu/top-menu';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [
+    TopMenu
+  ],
   styleUrl: './app.scss'
 })
 export class App {
--- pocAngular/src/app/components/top-menu/top-menu.ts
+++ pocAngular/src/app/components/top-menu/top-menu.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-top-menu',
+  standalone: true,
   imports: [],
   templateUrl: './top-menu.html',
   styleUrl: './top-menu.scss'
