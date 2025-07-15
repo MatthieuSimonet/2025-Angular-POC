ca0b5aa43c000a08447a856f8311b6e72b1f4b07
Simonet <matthieu.simonet@iut-dijon.u-bourgogne.fr>
2025-07-15 15:26:03 +0200

    [Metier] Code du métier du Menu du haut

--- pocAngular/src/app/components/top-menu/top-menu.ts
+++ pocAngular/src/app/components/top-menu/top-menu.ts
@@ -7,6 +7,32 @@ import { Component } from '@angular/core';
   templateUrl: './top-menu.html',
   styleUrl: './top-menu.scss'
 })
+
+
+//Menu du haut
 export class TopMenu {
 
+  //#region --- Attributs ---
+  private _titre : string;
+  private _boutons : string[];
+  //#endregion
+
+  //#region --- Propriétés ---
+  //Titre du menu
+  public get titre() : string {
+    return this._titre;
+  }
+
+  //Liste des boutons
+  public get boutons() : string[] {
+    return this._boutons;
+  }
+  //#endregion
+
+  //#region --- Constructeurs ---
+  public constructor() {
+    this._titre = "Mon Titre";
+    this._boutons = ["Bouton1","Bouton2"];
+  }
+  //#endregion
 }
